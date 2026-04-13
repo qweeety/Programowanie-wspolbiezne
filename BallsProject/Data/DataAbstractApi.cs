77825278796d182fb02475f0be42cbff21763450
Artem <artretyakov060@gmail.com>
2026-04-13 14:51:47 +0200

    dodalem DataAbstractApi oraz zaleznosci do Logic

---
+++ BallsProject/Data/DataAbstractApi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Data
+{
+    public abstract class DataAbstractApi
+    {
+        public abstract IBall CreateBall(double x, double y, double radius);
+        public abstract double Width { get; }
+        public abstract double Height { get; }
+
+        public static DataAbstractApi CreateApi(double width, double height)
+            => new DataApi(width, height);
+    }
+
+    internal class DataApi : DataAbstractApi
+    {
+        public override double Width { get; }
+        public override double Height { get; }
+
+        public DataApi(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override IBall CreateBall(double x, double y, double radius)
+            => new Ball(x, y, radius);
+    }
+}
