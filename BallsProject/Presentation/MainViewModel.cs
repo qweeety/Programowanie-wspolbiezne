f17cd1fcc66035bbfa68b4cc3108670822dde843
Artem <artretyakov060@gmail.com>
2026-04-20 14:48:08 +0200

    ostatnia wersja

---
+++ BallsProject/Presentation/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Logic;
+using Data;
+
+namespace Presentation.ViewModel
+{
+    public class MainViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        private readonly LogicAbstractApi _logic;
+        public ObservableCollection<IBall> Balls { get; } = new();
+        public ICommand StartCommand { get; }
+
+        public MainViewModel()
+        {
+            _logic = LogicAbstractApi.CreateApi();
+            StartCommand = new RelayCommand(() => {
+                _logic.CreateBalls(5);
+                Balls.Clear();
+                foreach (var b in _logic.GetBalls()) Balls.Add(b);
+                _logic.Start();
+            });
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly System.Action _execute;
+        public RelayCommand(System.Action execute) => _execute = execute;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => _execute();
+        public event System.EventHandler CanExecuteChanged;
+    }
+}
--- BallsProject/View/MainWindow.xaml.cs
+++ BallsProject/View/MainWindow.xaml.cs
@@ -8,17 +8,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Presentation.ViewModel;
 
 namespace View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel();
         }
     }
 }
